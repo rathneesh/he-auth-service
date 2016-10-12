eb6966c93ba9fed569ebadcc559e4ecf9a6a84a4
Ricardo Quintana <rqc@users.noreply.github.com>
2016-10-12 09:56:24 -0400

    Bugfix/pipeline npm test (#20)
    * Fixed npm run test and added npm run tdd.
    * Replaced jshint with eslint and added google standards (WIP - have not fixed errors / warnings).
    Referenced ./node_modules jenkins-mocha directly.

---
+++ test/stub.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+
+/* eslint-disable no-undef */
+
+describe('FooTest', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1, 2, 3].indexOf(4));
+    });
+  });
+});
+
